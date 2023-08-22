02ff079ec55bb82a519b722198180bdc8da14294
CarlangasDeveloper <130238051+CarlangasDeveloper@users.noreply.github.com>
2023-08-22 17:58:23 -0500

    Update Años_Bisiestos.java
    "Se corrigieron los condicionales y se hizo más eficiente el código"

--- Años_Bisiestos.java
+++ Años_Bisiestos.java
@@ -4,24 +4,14 @@ public class Años_Bisiestos {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("---Años Bisiestos---");
-        System.out.println("Ingrese un año para saber si es bisiesto o no: ");
+        System.out.print("Ingrese un año para saber si es bisiesto o no: ");
         int año = sc.nextInt();
 
-        if (año >= 1582) {
-            if (año % 400 == 0) {
-                System.out.println("El año " + año + " SI es bisiesto");
-            } else {
-                System.out.println("El año " + año + " NO es bisiesto");
-            }
+        if(((año % 4 == 0) && (año % 100 != 0)) || (año % 100 == 0) && (año % 400 == 0)){
+            System.out.println("El año "+año+" SI es bisiesto");
         }
-        else {
-            if(año % 4 == 0){
-                System.out.println("El año "+año+" SI es bisiesto");
-            }
-            else {
-                System.out.println("El año " + año + " NO es bisiesto");
-            }
+        else{
+            System.out.println("El año "+año+" NO es bisiesto");
         }
     }
 }
-
